a4ccc35f369fa8b0cac7d4a782c06d7cc12b4b05
Dearion7 <j.cuperus@live.nl>
2023-06-06 14:16:28 +0200

    Uptime Kuma upload
    Script to create a LXC in Proxmox that runs Uptime Kuma

---
+++ uptimekuma.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+
+function header_info {
+clear
+cat <<"EOF"
+   __  __      __  _                   __ __                     
+  / / / /___  / /_(_)___ ___  ___     / //_/_  ______ ___  ____ _
+ / / / / __ \/ __/ / __  __ \/ _ \   / ,< / / / / __  __ \/ __  /
+/ /_/ / /_/ / /_/ / / / / / /  __/  / /| / /_/ / / / / / / /_/ / 
+\____/ .___/\__/_/_/ /_/ /_/\___/  /_/ |_\__,_/_/ /_/ /_/\__,_/  
+    /_/                                                          
+ 
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Uptime Kuma"
+var_disk="4"
+var_cpu="1"
+var_ram="1024"
+var_os="debian"
+var_version="11"
+variables
+color
+catch_errors
+
+## Variables for creating a LXC Container
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="192.168.10.4/24"
+  GATE="192.168.10.1"
+  DISABLEIP6="yes"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN="10"
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+## install Uptime Kuma from github
+function update_script() {
+header_info
+if [[ ! -d /opt/uptime-kuma ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+LATEST=$(curl -sL https://api.github.com/repos/louislam/uptime-kuma/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+msg_info "Stopping ${APP}"
+sudo systemctl stop uptime-kuma &>/dev/null
+msg_ok "Stopped ${APP}"
+
+cd /opt/uptime-kuma
+
+msg_info "Pulling ${APP} ${LATEST}"
+git fetch --all &>/dev/null
+git checkout $LATEST --force &>/dev/null
+msg_ok "Pulled ${APP} ${LATEST}"
+
+msg_info "Updating ${APP} to ${LATEST}"
+npm install --production &>/dev/null
+npm run download-dist &>/dev/null
+msg_ok "Updated ${APP}"
+
+msg_info "Starting ${APP}"
+sudo systemctl start uptime-kuma &>/dev/null
+msg_ok "Started ${APP}"
+msg_ok "Updated Successfully"
+exit
+}
+
+start
+build_container
+description
+
+## Show URL IP information to show dashboard
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:3001${CL} \n"
